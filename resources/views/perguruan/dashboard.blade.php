8494bf226852dcf2643a8e4a38b9cf5f6e0b6a11
Nazaludin <ludinnaza@gmail.com>
2024-05-06 18:12:44 +0700

    fixing provinsi

--- resources/views/perguruan/dashboard.blade.php
+++ resources/views/perguruan/dashboard.blade.php
@@ -110,13 +110,37 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="max-w-sm">
+                            <!-- SearchBox -->
+                            <div class="relative" data-hs-combo-box='{
+    "isOpenOnFocus": true,
+    "apiUrl": "http:\\localhost:8000/api/provinsi",
+    "outputItemTemplate": "<div data-hs-combo-box-output-item><span class=\"flex items-center cursor-pointer py-2 px-4 w-full text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:text-neutral-200 dark:focus:bg-neutral-700\"><div class=\"flex items-center w-full\"><div class=\"flex items-center justify-center rounded-full bg-gray-200 size-6 overflow-hidden me-2.5\"><img class=\"flex-shrink-0\" data-hs-combo-box-output-item-attr=&apos;[{\"data\": \"provinsi\",}]&apos; /></div><div data-hs-combo-box-output-item-field=\"name\" data-hs-combo-box-search-text data-hs-combo-box-value></div></div><span class=\"hidden hs-combo-box-selected:block\"><svg class=\"flex-shrink-0 size-3.5 text-blue-600 dark:text-blue-500\" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"></polyline></svg></span></span></div>",
+  }'>
+                                <div class="relative">
+                                    <div class="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-3.5">
+                                        <svg class="flex-shrink-0 size-4 text-gray-400 dark:text-white/60" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                            <circle cx="11" cy="11" r="8"></circle>
+                                            <path d="m21 21-4.3-4.3"></path>
+                                        </svg>
+                                    </div>
+                                    <input class="py-3 ps-10 pe-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" type="text" placeholder="Type a name" value="" data-hs-combo-box-input="">
+                                </div>
 
+                                <!-- SearchBox Dropdown -->
+                                <div class="absolute z-50 w-full bg-white border border-gray-200 rounded-lg dark:bg-neutral-800 dark:border-neutral-700" style="display: none;" data-hs-combo-box-output="">
+                                    <div class="max-h-72 rounded-b-lg overflow-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500" data-hs-combo-box-output-items-wrapper=""></div>
+                                </div>
+                                <!-- End SearchBox Dropdown -->
+                            </div>
+                            <!-- End SearchBox -->
+                        </div>
                         <div class="divider">Tempat</div>
                         <!-- Form -->
                         <label for="provinsi" class="form-label">Povinsi</label>
                         <div class="relative mb-4" data-hs-combo-box="">
                             <div class="relative">
-                                <input placeholder="Povinsi..." id="provinsi" name="provinsi" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 disabled:opacity-50 disabled:pointer-events-none" type="text" value="jawa" data-hs-combo-box-input="" />
+                                <input placeholder="Povinsi..." id="provinsi" name="provinsi" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 disabled:opacity-50 disabled:pointer-events-none" type="text" value="" data-hs-combo-box-input="" />
                                 <div class="absolute top-1/2 end-3 -translate-y-1/2" data-hs-combo-box-toggle="">
                                     <svg class="flex-shrink-0 size-3.5 text-gray-500 dark:text-neutral-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                         <path d="m7 15 5 5 5-5"></path>
@@ -126,17 +150,34 @@
                             </div>
                             <x-input-error :messages="$errors->get('provinsi')" class="mt-2" />
                             <div id="cotainter_provinsi" class="absolute z-50 w-full max-h-72 p-1 bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500" style="display: none" data-hs-combo-box-output="">
+
+                                <!-- select -->
+                                <div class="cursor-pointer py-2 px-4 w-full text-sm text-main hover:text-white hover:bg-main rounded-lg focus:outline-none focus:opacity-75" tabindex="0" data-hs-combo-box-output-item="">
+                                    <div class="flex justify-between items-center w-full">
+                                        <span data-hs-combo-box-search-text="Argentina" data-hs-combo-box-value="">Argentina</span>
+                                        <span class="hidden hs-combo-box-selected:block">
+                                            <svg class="flex-shrink-0 size-3.5 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                                <path d="M20 6 9 17l-5-5"></path>
+                                            </svg>
+                                        </span>
+                                    </div>
+                                </div>
+                                <!-- select -->
+
                             </div>
                         </div>
                         <!-- Form End -->
 
                         <script type="module">
                             $(document).ready(function() {
+
+
                                 $(document).on('click', '#cotainter_provinsi [data-hs-combo-box-output-item]', function() {
                                     // Mengambil nilai teks dari opsi yang dipilih
                                     var selectedProvinsi = $(this).find('span[data-hs-combo-box-search-text]').text();
 
                                     // Mengatur nilai input #provinsi sesuai dengan opsi yang dipilih
+                                    // $('#provinsi').change().preventDefault();
                                     $('#provinsi').val(selectedProvinsi);
                                     console.log($('#provinsi').val());
                                     $('#provinsi').attr('data-hs-combo-box-input', selectedProvinsi);
@@ -180,9 +221,43 @@
                                         });
                                     } else {
                                         // Sembunyikan hasil pencarian jika input kosong
-                                        $('#cotainter_provinsi').hide();
+                                        // $('#cotainter_provinsi').hide();
                                     }
                                 });
+
+                                // // Memunculkan opsi ketika input field diklik
+                                // $('#provinsi').on('click', function() {
+                                //     $('[data-hs-combo-box-output]').show();
+                                // });
+
+                                // // Menyembunyikan opsi ketika klik dilakukan di luar combobox
+                                // $(document).on('click', function(event) {
+                                //     const $target = $(event.target);
+                                //     if (!$target.closest('[data-hs-combo-box]').length) {
+                                //         $('[data-hs-combo-box-output]').hide();
+                                //     }
+                                // });
+
+                                // // Menangani pemilihan opsi
+                                // $('[data-hs-combo-box-output]').on('click', '[data-hs-combo-box-output-item]', function() {
+                                //     const selectedPerson = $(this).find('[data-hs-combo-box-search-text]').text();
+                                //     $('#provinsi').val(selectedPerson);
+                                //     $('[data-hs-combo-box-output]').hide();
+                                // });
+
+                                // // Event listener to close combobox options when clicking outside the combobox
+                                // $(document).click(function(event) {
+                                //     const $target = $(event.target);
+                                //     if (!$target.closest('#cotainter_provinsi').length) {
+                                //         $('#cotainter_provinsi').hide();
+                                //     }
+                                // });
+
+                                // // Prevent closing options when clicking inside the combobox
+                                // $('#cotainter_provinsi').click(function(event) {
+                                //     event.stopPropagation();
+                                // });
+
                             });
                         </script>
 
