1399bab34d0e2bea5275c81aa7cb2d1b32f00076
Rianhahaha <triandiapriliio@gmail.com>
2024-05-13 01:48:55 +0700

    menambahkan edit

--- resources/views/adminpg/prestasi/index.blade.php
+++ resources/views/adminpg/prestasi/index.blade.php
@@ -67,7 +67,7 @@
 
                 <div class="p-6 text-gray-900">
                     <div class="divider">Daftar Prestasi</div>
-                    @if (!$exist))
+                    @if (!$exist)
                     <div class="flex flex-wrap items-center justify-center h-full">
                         <div class="flex flex-col gap-5 text-center justify-center items-center md:items-start md:text-left">
                             <p class="font-bold text-3xl text-center">Data yang kamu cari tidak ada nih!</p>
@@ -89,7 +89,7 @@
                                         <div>
                                             <label for="hs-trailing-button-add-on-with-icon-and-button" class="sr-only">Label</label>
                                             <div class="relative flex rounded-lg shadow-sm my-2 mx-2">
-                                                <input type="text" id="search" name="hs-trailing-button-add-on-with-icon-and-button" class="py-3 px-4 ps-11 block w-full border-gray-200 shadow-sm rounded-s-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
+                                                <input placeholder="Cari Prestasi..." type="text" id="search" name="hs-trailing-button-add-on-with-icon-and-button" class="py-3 px-4 ps-11 block w-full border-gray-200 shadow-sm rounded-s-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
                                                 <div class="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
                                                     <svg class="flex-shrink-0 size-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                                         <circle cx="11" cy="11" r="8"></circle>
